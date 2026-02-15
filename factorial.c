aa0e15d8a7713364c4c1924c0448423bea135c92
MANAS MISHRA <manasmishra1405@gmail.com>
2026-02-15 19:34:32 +0530

    Add Palindrome checker program in C

---
+++ factorial.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, original, reverse = 0, remainder;
+
+    printf("Enter the number:\n");
+    if (scanf("%d", &num) != 1) {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    original = num;
+
+    /* negative numbers are not treated as palindromes here */
+    if (num < 0) {
+        printf("%d is not a palindrome number\n", original);
+        return 0;
+    }
+
+    while (num != 0)
+    {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+        num /= 10;
+    }
+
+    if (original == reverse)
+        printf("%d is a palindrome number\n", original);
+    else
+        printf("%d is not a palindrome number\n", original);
+
+    return 0;
+}
