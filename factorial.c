aa08a626514071faa73669a51205c882aa4ac4b6
MANAS MISHRA <manasmishra1405@gmail.com>
2026-02-16 22:16:19 +0530

    Add factorial program in C

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i,n;
+    long fact = 1;
+    printf("Enter a number:\n");
+    scanf("%d", &n);
+    for (i=0; i<n ;i++)
+    {
+        fact = fact * (i+1);
+    }
+    printf("Factorial of %d is %d\n", n, fact);
+    return 0;
+}
